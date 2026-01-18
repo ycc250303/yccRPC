04f4e5146b9e477dca43703e68b397edcac6c9b0
ycc250303 <yin2533@outlook.com>
2026-01-19 00:21:10 +0800

    feat：实现 ZooKeeper 注册中心

---
+++ code/ycc-core-rpc/src/test/java/com/code/registry/ZookeeperRegistryTest.java
@@ -0,0 +1,70 @@
+package com.code.registry;
+
+import com.code.config.RegistryConfig;
+import com.code.model.ServiceMetaInfo;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Zookeeper 注册中心测试
+ */
+public class ZookeeperRegistryTest {
+
+    final Registry registry = new ZooKeeperRegistry();
+
+    @Before
+    public void init() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        // 确保你的 Zookeeper 运行在此地址
+        registryConfig.setAddress("localhost:2181");
+        registry.init(registryConfig);
+    }
+
+    @Test
+    public void register() throws Exception {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1234);
+        registry.register(serviceMetaInfo);
+        
+        serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1235);
+        registry.register(serviceMetaInfo);
+        
+        serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("2.0");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1234);
+        registry.register(serviceMetaInfo);
+    }
+
+    @Test
+    public void unRegister() {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        serviceMetaInfo.setServiceHost("localhost");
+        serviceMetaInfo.setServicePort(1234);
+        registry.unRegister(serviceMetaInfo);
+    }
+
+    @Test
+    public void serviceDiscovery() {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        String serviceKey = serviceMetaInfo.getServiceKey();
+        List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceKey);
+        Assert.assertNotNull(serviceMetaInfoList);
+        System.out.println("发现服务列表: " + serviceMetaInfoList);
+    }
+}
